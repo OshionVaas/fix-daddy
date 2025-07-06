856232ba69cda4a067386f0ab1948b25823a3ba5
Oshion Vaas <oshion.vaas92@gmail.com>
2025-07-06 17:50:53 +0530

    first change

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className="bg-gray-900 text-white py-6 text-center text-sm space-y-2">
       <div>
         <Link to="/admin-login" className="hover:underline mx-3">🔐 Admin Login</Link>
-        <Link to="/technician-login" className="hover:underline mx-3">🔧 Technician Login</Link>
+        <Link to="/technician-login" className="hover:underline mx-3">Technician Login</Link>
       </div>
       <div>&copy; {new Date().getFullYear()} FixDaddyShop. All rights reserved.</div>
     </footer>
