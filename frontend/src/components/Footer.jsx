f25a30020ce016924daa8974d4893a65b910fb00
Oshion Vaas <oshion.vaas92@gmail.com>
2025-09-04 09:35:23 +0530

    Updated App.jsx and Footer.jsx, modified package.json and lockfile

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,14 +1,13 @@
+// src/components/Footer.jsx
 import React from "react";
-import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <footer className="bg-gray-900 text-white py-6 text-center text-sm space-y-2">
       <div>
-        <Link to="/admin-login" className="hover:underline mx-3">🔐 Admin Login</Link>
-        <Link to="/technician-login" className="hover:underline mx-3">Technician Login</Link>
+        {/* Removed Admin/Technician login links */}
+        <span className="mx-3">© {new Date().getFullYear()} FixDaddyShop. All rights reserved.</span>
       </div>
-      <div>&copy; {new Date().getFullYear()} FixDaddyShop. All rights reserved.</div>
     </footer>
   );
 };
